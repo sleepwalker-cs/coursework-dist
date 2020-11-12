cad7255630c2b7d31cfa87ffa52fb9e22db91072
sleepwalker-cs <74249529+sleepwalker-cs@users.noreply.github.com>
2020-11-12 04:18:55 +0200

    Add files via upload

---
+++ precache-manifest.9c5438809e7f4c1c8c6ec4ac272c2437.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd033d7bcd7af217b9e4",
+    "url": "/coursework-dist/css/app.1e326ca7.css"
+  },
+  {
+    "revision": "4da9863ad0178850107a",
+    "url": "/coursework-dist/css/chunk-0bba42c4.db20d8c4.css"
+  },
+  {
+    "revision": "f7a7145a16168c4ba00a",
+    "url": "/coursework-dist/css/chunk-15d93860.13ae0616.css"
+  },
+  {
+    "revision": "c2fa3e29a379a2244557",
+    "url": "/coursework-dist/css/chunk-77a0119c.13ae0616.css"
+  },
+  {
+    "revision": "9e3ee76581edad9ebf56",
+    "url": "/coursework-dist/css/chunk-vendors.73e1f5b0.css"
+  },
+  {
+    "revision": "0ab813572793b19418f6e16222a28b65",
+    "url": "/coursework-dist/index.html"
+  },
+  {
+    "revision": "dd033d7bcd7af217b9e4",
+    "url": "/coursework-dist/js/app.9930aacf.js"
+  },
+  {
+    "revision": "f3657f29d690dc1ff9ef",
+    "url": "/coursework-dist/js/chunk-00b817ff.aeaf9d25.js"
+  },
+  {
+    "revision": "4da9863ad0178850107a",
+    "url": "/coursework-dist/js/chunk-0bba42c4.98d6c4de.js"
+  },
+  {
+    "revision": "f7a7145a16168c4ba00a",
+    "url": "/coursework-dist/js/chunk-15d93860.9fda435f.js"
+  },
+  {
+    "revision": "910d47d18f28f8dbd466",
+    "url": "/coursework-dist/js/chunk-28bcbddf.44b1c903.js"
+  },
+  {
+    "revision": "d736ecdf53990b6aa10a",
+    "url": "/coursework-dist/js/chunk-2d217a99.e10e7747.js"
+  },
+  {
+    "revision": "79b7998f9a722b3b35f0",
+    "url": "/coursework-dist/js/chunk-3a4e8dfe.9786ea35.js"
+  },
+  {
+    "revision": "86a5c55502f9c7e85e19",
+    "url": "/coursework-dist/js/chunk-7566fda1.7a52d6d3.js"
+  },
+  {
+    "revision": "c2fa3e29a379a2244557",
+    "url": "/coursework-dist/js/chunk-77a0119c.a5252b8b.js"
+  },
+  {
+    "revision": "9e3ee76581edad9ebf56",
+    "url": "/coursework-dist/js/chunk-vendors.640a70fb.js"
+  },
+  {
+    "revision": "ab8ce35550750cbdffd12ae21bb91a63",
+    "url": "/coursework-dist/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/coursework-dist/robots.txt"
+  }
+]);
